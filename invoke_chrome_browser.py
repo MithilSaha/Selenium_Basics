065eefcf6c8ca96c8cc77581464bfa774d5c34a2
Mithil Saha <mithilsaha1997@gmail.com>
2023-04-29 00:43:15 +0530

    added browser invokation script

---
+++ invoke_chrome_browser.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(options = options)
+
+driver.get("https://www.amazon.in/")
+
+driver.quit()
